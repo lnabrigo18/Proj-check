012a7e8217513ea2cf5553497ac15ba4229a2ce3
lnabrigo18 <lnabrigo@gmail.com>
2021-06-01 20:54:27 +0800

    add devise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    before_action :get_category, only [ :create, :edit, :update, :destroy ] 
+    before_action :get_category, only: [ :create, :edit, :update, :destroy ] 
 
     def show
         @task = Task.find(params[:id])
